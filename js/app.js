bc0f2d4df419b52b5446b237b6c886d9a0aae925
V914eslav <v914-working@rambler.ru>
2022-01-29 19:13:35 +0300

    add data-scroll and toggleBurger

--- js/app.js
+++ js/app.js
@@ -1,16 +1,41 @@
 $(function () {
+  // Fixed Header
   let header = $("#header");
   let banner = $("#banner");
   let scrollPos = $(window).scrollTop();
+  let bannerHeight = banner.innerHeight();
+
+  checkScroll(scrollPos, bannerHeight);
 
   $(window).on("scroll load resize", function () {
-    let bannerHeight = banner.innerHeight();
+    bannerHeight = banner.innerHeight();
     scrollPos = $(this).scrollTop();
+    checkScroll(scrollPos, bannerHeight);
+  });
+
+  function checkScroll(scrollPos, bannerHeight) {
     if (scrollPos > bannerHeight) {
       header.addClass("fixed");
     } else {
       header.removeClass("fixed");
     }
-    console.log(scrollPos);
+  }
+  let nav = $("#nav");
+  let navToggle = $("#navToggle");
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
+    let elementId = $(this).data("scroll");
+    let elementOffset = $(elementId).offset().top;
+    nav.removeClass("show");
+    $("html, body").animate(
+      {
+        scrollTop: elementOffset - 70,
+      },
+      700
+    );
+  });
+  navToggle.on("click", function (event) {
+    event.preventDefault();
+    nav.toggleClass("show");
   });
 });
